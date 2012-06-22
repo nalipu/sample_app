ce1745ff2aca358b5f5c5a9205cc7f4aafc58c26
nalipu <nalipu@web.de>
2012-06-22 17:24:03 +0200

    Finish user edit, update, index and destroy

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "User"
-    email "user@mail.de"
+    sequence(:name) { |n| "person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
   end
 end
